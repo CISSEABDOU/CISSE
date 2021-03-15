d71656439be5f7f54483557672678d1a45ceb0b0
cisse <abdou.aziz.cisse@etu.univ-st-etienne.fr>
2021-03-15 08:51:14 +0100

    modifications seance 6

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 use App\Entity\Utilisateur;
-Use App\Form\InscriptionType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\component\Validator\Constraints\DateTime;
+
 class ProjectController extends AbstractController
 {
     /**
@@ -19,18 +21,45 @@ class ProjectController extends AbstractController
             'controller_name' => 'ProjectController',
         ]);
     }
+	/**
+     * @Route("/listeutilisateur", name="listeutilisateur")
+     */
+    public function listeutilisateur(): Response
+    {
+        return $this->render('project/listeutilisateur.html.twig', [
+            'controller_name' => 'ProjectController',
+        ]);
+    }
 	
 	
 	 /**
      * @Route("/registration", name="registration")
      */
-    public function registration()
+    public function registration(LoggerInterface $logeer,Request $request,EntityManagerInterface $manager):response
     {
-		$user = new Utilisateur();
-		$form = $this->createForm(InscriptionType::class, $user);
-        return $this->render('project/registration.html.twig', [ 
-		    'form' => $form->createView()
-		]);
+		// Récupération des données saisies
+		$Nom=$request->request->get("nom");
+		$Prenom=$request->request->get("prenom");
+		$Email=$request->request->get("email");
+		$Mot_de_passe=$request->request->get("password");
+		$id=$request->request->get("id");
+		
+		if ($Nom=="admin")
+		   $message="Vous êtes admin";
+		else
+		   $message="Vous êtes utilisateur";
+		
+		//Création d'un objet utilisateur
+		
+		$monUtilisateur = new Utilisateur();
+		
+		$monUtilisateur -> setNom($Nom);
+		$monUtilisateur -> setPrenom($Prenom);
+		$monUtilisateur -> setEmail($Email);
+		$monUtilisateur -> setMotDePasse($Mot_de_passe);
+		
+		//Sotcker l'objet dans la base de données avec la persistance
+		$manager->persist($monUtilisateur);
+        $manager->flush();
     }
-	
 }
